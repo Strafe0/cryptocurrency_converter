ddf7a68ed1baf31dcbe991bea84c14939d2aee14
ValeriyGolyshev <valeriy1golyshev@gmail.com>
2024-10-20 14:06:58 +0300

    Added GetCurrencies use case

---
+++ lib/domain/usecases/get_currencies.dart
@@ -0,0 +1,12 @@
+import 'package:cryptocurrency_converter/data/repositories/currency_repository.dart';
+import 'package:cryptocurrency_converter/domain/entities/currency/currency.dart';
+
+class GetCurrencies {
+  GetCurrencies(this._currencyRepository);
+
+  final CurrencyRepository _currencyRepository;
+
+  Future<List<Currency>?> call() async {
+    return await _currencyRepository.getCurrencies();
+  }
+}
